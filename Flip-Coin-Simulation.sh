55dc6ee58f30056b2e27cc6f9c5f2918bfafef8d
unknown <varadesharad@gmail.com>
2020-04-20 11:24:57 +0530

    Added:single H or T or calc percentage

--- Flip-Coin-Simulation.sh
+++ Flip-Coin-Simulation.sh
@@ -1,8 +1,44 @@
 #!/bin/bash -x
 
-if [ $((RANDOM%2)) -eq 1 ];
-then
-		echo "H"
-else
-		echo	"T"
-fi
+count=0
+
+function flipCoin() {
+	if [ $((RANDOM%2)) -eq 1 ];
+	then
+			echo "H"
+	else
+			echo	"T"
+	fi
+}
+
+function getSingletPercentage() {
+	for key in ${!singletCombination[@]}
+	do
+		value=${singletCombination[$key]}
+		percentage[$key]=$((value*100/numberOfTime))
+
+	done
+}
+
+
+function storeSingletCombination() {
+singletCombination["H"]=0
+singletCombination["T"]=0
+while [ $count != $numberOfTime ]
+do
+
+	result=$( flipCoin )
+	value=${singletCombination[$result]}
+	singletCombination[$result]=$((++value))
+	((count++))
+
+done
+}
+
+read -p "Enter the number of Time: " numberOfTime
+storeSingletCombination
+echo ${singletCombination[@]}
+echo ${!singletCombination[@]}
+getSingletPercentage
+echo ${percentage[@]}
+echo ${!percentage[@]}
